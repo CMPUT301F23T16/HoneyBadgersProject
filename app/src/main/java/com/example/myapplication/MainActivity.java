586f4600bba3d3b51f76bafbf2a70bbbb0b3ec9f
Aditya Goyal <agoyal7000@gmail.com>
2023-11-05 23:24:55 -0700

    Adds ability to view a user's items via database

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+
+/**
+ * Setup the database for the application.
+ * Populate the screen with all the items.
+ */
+public class MainActivity extends AppCompatActivity implements DataBase.ItemListUpdateListener {
+
+    private RecyclerView itemListView;
+    private ItemArrayAdapter itemListAdapter;
+    private DataBase db; //Source of item list
+
+    /**
+     * When activity is created, setup database and user's items
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.item_list_activity);
+
+        //Data base instance with userName
+        db = new DataBase("Adi", this);
+
+        //Create the viewable item list
+        itemListView = findViewById(R.id.item_list);
+        itemListView.setLayoutManager(new LinearLayoutManager(this));
+        itemListAdapter = new ItemArrayAdapter(this, db.getItemList());
+        itemListView.setAdapter(itemListAdapter);
+    }
+
+    /**
+     * Updates the screen when the itemList is changed in teh database.
+     * This can happen due to factors internal or external to this application.
+     */
+    public void onItemListUpdate(){
+        itemListAdapter.notifyDataSetChanged();
     }
 }
+
