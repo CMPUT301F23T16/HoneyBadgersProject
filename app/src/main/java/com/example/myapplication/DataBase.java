3476a9d0177179a7685c723969da3028650a6e73
Fahrin Bushra <87880319+Fahrin0802@users.noreply.github.com>
2023-11-07 13:37:30 -0700

    Add Item fuctionality implemented

---
+++ app/src/main/java/com/example/myapplication/DataBase.java
@@ -0,0 +1,113 @@
+package com.example.myapplication;
+
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**This class represents the cloud database the app is connecting to.
+ * All communications with the database are handled here.
+ */
+public class DataBase {
+    private FirebaseFirestore db;
+    private ArrayList<Item> itemList;
+    private CollectionReference itemsRef;
+
+    /**
+     * Connect to the database and get the user's collection. Setup automatic
+     * updates from database
+     * @param userName This is the username of the user
+     * @param context Parent object
+     */
+    public DataBase(String userName, ItemListActivity context) {
+        this.itemList = new ArrayList<>();
+
+        this.db = FirebaseFirestore.getInstance();
+        this.itemsRef = db.collection(userName); // Each user has their own collection
+
+        itemsRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onEvent(@Nullable QuerySnapshot querySnapshots,
+                                @Nullable FirebaseFirestoreException error) {
+                if (error != null) {
+                    Log.e("Firestore", error.toString());
+                    return;
+                }
+                if (querySnapshots != null) {
+                    itemList.clear();
+                    //For each item in the cloud db, add it to the list
+                    for (QueryDocumentSnapshot doc: querySnapshots) {
+
+                        String name = doc.getId();
+                        Date date;
+                        try {
+                            date = new SimpleDateFormat("yyyy-MM-dd").parse(doc.getString("dateAdded"));
+                        } catch (ParseException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                        Double price = doc.getDouble("price");
+                        String description = doc.getString("description");
+                        String make = doc.getString("make");
+                        String model = doc.getString("model");
+                        String serial = doc.getString("serial");
+                        String comment = doc.getString("comment");
+                        String tag = doc.getString("tag");
+
+                        Item temp = new Item(name, price, date, description, make,
+                                model, serial, comment, tag);
+
+                        Log.d("Firestore", String.format("Item(%s, %s, %s) fetched",
+                                name, doc.getString("dateAdded"), price));
+                        itemList.add(temp);
+                    }
+
+                    //This will notify any listeners via ItemListUpdateListener interface
+                    context.onItemListUpdate();
+                }
+            }
+        });
+    }
+
+
+    /**
+     * itemList getter
+     * @return list containing all items in database.
+     * This will be updated automatically as the list changes
+     */
+    public ArrayList<Item> getItemList() {
+        return itemList;
+    }
+
+    public void addItem(Item item){
+//        itemList.add(item);
+        itemsRef.document(item.getName()).set(item);
+    }
+
+    /**
+     * Interface for itemList listeners
+     */
+    public interface ItemListUpdateListener {
+        /**
+         * This method is called whenever an update to itemList happens
+         */
+        public void onItemListUpdate();
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/Item.java
@@ -0,0 +1,165 @@
+package com.example.myapplication;
+
+import android.os.Build;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+/**
+ * This class represents an item.
+ */
+public class Item implements Serializable {
+    private String name;
+    private double price;
+    private String dateAdded;
+    private String description;
+    private String make;
+    private String model;
+    private String serial;
+    private String comment;
+    private String tag;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String strDate = dateFormat.format(new Date());
+
+        this.dateAdded = strDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    /**
+     * This constructor creates a new Item
+     * @param name This is the name of the item
+     * @param price This is the price of the item
+     * @param dateAdded This is the date the item was added to the database
+     */
+    public Item(String name, double price, Date dateAdded) {
+        this.name = name;
+        this.price = price;
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String strDate = dateFormat.format(new Date());
+
+        this.dateAdded = strDate;
+    }
+
+    /**
+     * This constructor creates a new Item using today's date
+     * @param name This is the name of the item
+     * @param price This is the price of the item
+     */
+    public Item(String name, double price) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+        String strDate = dateFormat.format(new Date());
+
+        this.name = name;
+        this.price = price;
+        this.dateAdded = strDate;
+    }
+
+    /**
+     * This constructor creates a new Item using all fields in Add Item Fragment
+     */
+    public Item(String name, double price, Date dateAdded, String description, String make,
+                String model, String serial, String comment, String tag) {
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String strDate = dateFormat.format(dateAdded);
+
+        this.name = name;
+        this.price = price;
+        this.dateAdded = strDate;
+        this.description = description;
+        this.make = make;
+        this.model = model;
+        this.serial = serial;
+        this.comment = comment;
+        this.tag = tag;
+    }
+
+    /**
+     * getter for name
+     * @return The name of the item
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * getter for date added
+     * @return The addition date of the item
+     */
+    public String getDateAdded() {
+        return this.dateAdded;
+    }
+
+    /**
+     * getter for price
+     * @return The price of the item
+     */
+    public Double getPrice() {
+        return this.price;
+    }
+}
