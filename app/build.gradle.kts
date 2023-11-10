1a72881bb75afbe84c7f241ba410d00d520fc2ab
Mohammad Shahriar Hossain <mhossai6@ualberta.ca>
2023-11-10 13:59:49 -0700

    Modified sign up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
     implementation("com.google.firebase:firebase-firestore")
+    implementation("androidx.annotation:annotation:1.2.0")
+    implementation("com.google.firebase:firebase-auth:22.2.0") // Use the latest version
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/myapplication/LoginSignupActivity.java
+++ app/src/main/java/com/example/myapplication/LoginSignupActivity.java
@@ -1,11 +1,30 @@
 package com.example.myapplication;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Calendar;
+
 
 /**
  * Provides a user interface for both login and signup processes.
@@ -17,6 +36,14 @@ public class LoginSignupActivity extends AppCompatActivity {
     private LinearLayout layoutLogin, layoutSignup;
     private Button buttonLogin, buttonSignup;
 
+    // Add EditText member variables for signup fields
+    private EditText signupFirstName, signupLastName, signupDateOfBirth,
+            signupUsername, signupEmail, signupCreatePassword, signupRepeatPassword;
+
+    // Add Firebase Firestore instance
+    private FirebaseFirestore firestoreDB;
+    private FirebaseAuth firebaseAuth;
+
     /**
      * Called when the activity is starting. This is where most initialization should go.
      * @param savedInstanceState If the activity is being re-initialized after previously being shut down
@@ -27,13 +54,34 @@ public class LoginSignupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_signup); // make sure the name matches your XML file name
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        firestoreDB = FirebaseFirestore.getInstance();
+
         layoutLogin = findViewById(R.id.layoutLogin);
         layoutSignup = findViewById(R.id.layoutSignup);
         buttonLogin = findViewById(R.id.buttonLogin);
         buttonSignup = findViewById(R.id.buttonSignup);
 
+        signupFirstName = findViewById(R.id.signupFirstName);
+        signupLastName = findViewById(R.id.signupLastName);
+        signupDateOfBirth = findViewById(R.id.signupDateOfBirth);
+        signupUsername = findViewById(R.id.signupUsername);
+        signupEmail = findViewById(R.id.signupEmail);
+        signupCreatePassword = findViewById(R.id.signupCreatePassword);
+        signupRepeatPassword = findViewById(R.id.signupRepeatPassword);
         // Initially show the login form
         showLoginForm();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if (currentUser != null) {
+            // User is signed in, navigate to the ItemListActivity
+            navigateToItemListActivity();
+        }
     }
 
     /**
@@ -52,14 +100,55 @@ public class LoginSignupActivity extends AppCompatActivity {
         showSignupForm();
     }
 
+    public void onDateOfBirthClicked(View view) {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                LoginSignupActivity.this,
+                (datePicker, selectedYear, selectedMonth, selectedDay) -> {
+                    selectedMonth = selectedMonth + 1; // Month is 0-based in Calendar
+                    String formattedDate = selectedYear + "-" +
+                            String.format("%02d", selectedMonth) + "-" +
+                            String.format("%02d", selectedDay);
+                    ((EditText) view).setText(formattedDate);
+                }, year, month, dayOfMonth);
+
+        datePickerDialog.show();
+    }
     /**
      * Handles the logic when the login form is submitted. Placeholder for actual implementation.
      * @param view The view that was clicked.
      */
     public void onLoginSubmitClicked(View view) {
-        // will implement login logic here
-        // For now, just a placeholder implementation
-        navigateToItemListActivity();
+        EditText loginEmail = findViewById(R.id.loginEmail);
+        EditText loginPassword = findViewById(R.id.loginPassword);
+
+        String email = loginEmail.getText().toString().trim();
+        String password = loginPassword.getText().toString().trim();
+
+        // Basic validation
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter both email and password.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Perform login using Firebase Auth (assuming username is an email)
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign-in success
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                        navigateToItemListActivity();
+                    } else {
+                        // If sign-in fails, display a message to the user.
+                        Toast.makeText(LoginSignupActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     /**
@@ -67,9 +156,81 @@ public class LoginSignupActivity extends AppCompatActivity {
      * @param view The view that was clicked.
      */
     public void onSignupSubmitClicked(View view) {
-        // will implement signup logic here
-        // For now, just a placeholder implementation
-        navigateToItemListActivity();
+        // Validate input fields are filled
+        if (validateSignupFields()) {
+            // Save data to Firebase Firestore
+            saveUserDataToFirebase();
+        } else {
+            // Show error to the user indicating that all fields are required
+            Toast.makeText(this, "Please fill in all fields.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * Validates that all signup fields are filled.
+     * @return true if all fields are filled, false otherwise.
+     */
+    private boolean validateSignupFields() {
+        // Check that none of the EditText fields are empty
+        return !signupFirstName.getText().toString().isEmpty() &&
+                !signupLastName.getText().toString().isEmpty() &&
+                !signupDateOfBirth.getText().toString().isEmpty() &&
+                !signupUsername.getText().toString().isEmpty() &&
+                !signupEmail.getText().toString().isEmpty() &&
+                !signupCreatePassword.getText().toString().isEmpty() &&
+                !signupRepeatPassword.getText().toString().isEmpty();
+    }
+
+    /**
+     * Saves the user's data to Firebase Firestore.
+     */
+    private void saveUserDataToFirebase() {
+        String email = signupEmail.getText().toString().trim();
+        String password = signupCreatePassword.getText().toString().trim();
+        String confirmPassword = signupRepeatPassword.getText().toString().trim();
+
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(this, "Passwords do not match.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Create user with email and password in Firebase Auth
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                        if (user != null) {
+                            Map<String, Object> userDetail = new HashMap<>();
+                            userDetail.put("firstName", signupFirstName.getText().toString().trim());
+                            userDetail.put("lastName", signupLastName.getText().toString().trim());
+                            userDetail.put("dateOfBirth", signupDateOfBirth.getText().toString().trim());
+                            // Do not save the password to Firestore
+                            // Remove the username if you are not using it
+
+                            // Store user details in Firestore
+                            firestoreDB.collection("users")
+                                    .document(user.getUid())
+                                    .set(userDetail)
+                                    .addOnSuccessListener(aVoid -> {
+                                        Log.d("Firebase", "User details updated in Firestore");
+                                        navigateToItemListActivity();
+                                    })
+                                    .addOnFailureListener(e -> {
+                                        Log.w("Firebase", "Error updating user details", e);
+                                        Toast.makeText(LoginSignupActivity.this,
+                                                "Failed to update user details: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                                    });
+                        }
+                    } else {
+                        // If sign up fails, display a message to the user.
+                        if (task.getException() != null) {
+                            Log.w("FirebaseAuth", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginSignupActivity.this,
+                                    "Authentication failed: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
     /**
