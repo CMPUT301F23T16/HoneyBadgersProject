4264efaac89266da1114062dc24acd3c5f5b8dde
Fahrin Bushra <87880319+Fahrin0802@users.noreply.github.com>
2023-12-02 10:27:24 -0500

    FIX build.gradle.kts
    - Should work now
    - But need espresso-contrib for interacting with stuff like calendar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,12 +42,25 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("androidx.annotation:annotation:1.2.0")
     implementation("com.google.firebase:firebase-auth:22.2.0") // Use the latest version
+    implementation("junit:junit:4.13.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test:runner:1.4.0")
     androidTestImplementation("androidx.test:rules:1.4.0")
     androidTestImplementation("com.google.android.material:material:1.10.0")
-    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
+//    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1"){
+//
+//        // IMPORTANT
+//        // !!! If using espresso-contrib,
+//        // !!! This module needs to be excluded for our UI errors to go away //
+//        /**
+//         * https://stackoverflow.com/questions/72558160/why-am-i-getting-internal-error-in-cloud-firestore-24-1-2-in-my-android-app-o
+//         * https://stackoverflow.com/questions/66338416/internal-error-in-cloud-firestore-22-1-0-when-running-instrumentation-test
+//         */
+//         exclude(module = "protobuf-lite")
+//    }
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.firebase:firebase-firestore")
     androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
 }
