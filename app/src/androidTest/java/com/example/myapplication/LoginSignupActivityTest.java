12e6297e27ea88a155ce6e816d12307094ea455b
Aditya Goyal <agoyal7000@gmail.com>
2023-12-03 15:57:06 -0700

    Adds missing import

--- app/src/androidTest/java/com/example/myapplication/LoginSignupActivityTest.java
+++ app/src/androidTest/java/com/example/myapplication/LoginSignupActivityTest.java
@@ -14,6 +14,7 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
