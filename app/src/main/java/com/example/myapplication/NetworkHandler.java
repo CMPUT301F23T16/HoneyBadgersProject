eac6ffb66e9d655dd4d1ce0e951a1f5df357eeee
Aditya Goyal <agoyal7000@gmail.com>
2023-12-03 17:29:10 -0700

    Refactors Network handler

--- app/src/main/java/com/example/myapplication/NetworkHandler.java
+++ app/src/main/java/com/example/myapplication/NetworkHandler.java
@@ -11,31 +11,49 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class NetworkHandler {
-    public String performRequest(String urlString) throws IOException {
-        HttpURLConnection urlConnection = null;
+    private static final String API_KEY = "ad5c40df65mshba2972e2b4bd264p1df5f1jsnae362888b3bb";
+
+    private HttpURLConnection setupConnection(URL url) {
+        HttpURLConnection urlConnection;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setRequestProperty("X-RapidAPI-Key", API_KEY);
+            return urlConnection;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    private String readResponse(HttpURLConnection connection) throws IOException {
         StringBuilder result = new StringBuilder();
+
+        InputStream inStream = new BufferedInputStream(connection.getInputStream());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inStream));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            result.append(line);
+        }
+
+        return result.toString();
+    }
+    public String performRequest(String urlString) throws IOException {
+        String result;
+        HttpURLConnection urlConnection = null;
 
         try {
             URL url = new URL(urlString);
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.setRequestProperty("X-RapidAPI-Key", "ad5c40df65mshba2972e2b4bd264p1df5f1jsnae362888b3bb");
-            // Add more headers or configuration as needed
+            urlConnection = setupConnection(url);
+            result = readResponse(urlConnection);
 
-            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                result.append(line);
-            }
         } finally {
             if (urlConnection != null) {
                 urlConnection.disconnect();
             }
         }
 
-        Log.d("API response", result.toString());
-        return result.toString();
+        Log.d("API response", result);
+        return result;
     }
 }
